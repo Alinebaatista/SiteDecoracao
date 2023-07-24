fae142675e8cbc9ab26be6c9afc7c51fd81c8d58
Aline Batista <102964288+Alinebaatista@users.noreply.github.com>
2023-07-24 19:58:16 -0300

    Add files via upload

---
+++ DreamParty/image.js
@@ -0,0 +1,124 @@
+var image = document.getElementById("playerCustomizado");
+
+
+function Ampliar(){
+    image.width = 500;
+}
+
+function Reduzir(){
+    image.width = 400;
+}
+
+//imagem 2
+
+var image1 = document.getElementById("playerCustomizado1");
+
+
+function Ampliar1(){
+    image1.width = 500;
+}
+
+function Reduzir1(){
+    image1.width = 400;
+}
+
+//imagem 3
+
+var image2 = document.getElementById("playerCustomizado2");
+
+
+function Ampliar2(){
+    image2.width = 500;
+}
+
+function Reduzir2(){
+    image2.width = 400;
+}
+
+//imagem 4
+
+var image3 = document.getElementById("playerCustomizado3");
+
+
+function Ampliar3(){
+    image3.width = 500;
+}
+
+function Reduzir3(){
+    image3.width = 400;
+}
+
+//imagem 5
+
+var image5 = document.getElementById("playerCustomizado5");
+
+
+function Ampliar5(){
+    image5.width = 500;
+}
+
+function Reduzir5(){
+    image5.width = 400;
+}
+
+
+//imagem 6
+var image4 = document.getElementById("playerCustomizado4");
+
+
+function Ampliar4(){
+    image4.width = 500;
+}
+
+function Reduzir4(){
+    image4.width = 400;
+}
+
+
+//imagem 6
+var image6 = document.getElementById("playerCustomizado6");
+
+
+function Ampliar6(){
+    image6.width = 500;
+}
+
+function Reduzir6(){
+    image6.width = 400;
+}
+
+//imagem 7
+var image7 = document.getElementById("playerCustomizado7");
+
+
+function Ampliar7(){
+    image7.width = 500;
+}
+
+function Reduzir7(){
+    image7.width = 400;
+}
+
+//imagem 8
+var image8 = document.getElementById("playerCustomizado8");
+
+
+function Ampliar8(){
+    image8.width = 500;
+}
+
+function Reduzir8(){
+    image8.width = 400;
+}
+
+//imagem 9
+var image9 = document.getElementById("playerCustomizado9");
+
+
+function Ampliar9(){
+    image9.width = 500;
+}
+
+function Reduzir9(){
+    image9.width = 400;
+}
---
+++ DreamParty/script.js
@@ -0,0 +1,224 @@
+var meuVideo =  document.getElementById("playerCustomizado"); 
+
+
+function playPause() {
+    if(meuVideo.paused)
+     meuVideo.play();
+    else
+    meuVideo.pause();
+}
+
+function ampliar() {
+    meuVideo.width = 650;
+}
+
+function reduzir() {
+    meuVideo.width= 350;
+}
+
+function telaCheia() {
+    meuVideo.requestFullscreen ();
+}
+function tamanhoNormal() {
+    meuVideo.width=500;
+}
+
+function aumentarVolume() {
+    meuVideo.volume +=0.1;
+}
+
+function diminuirVolume() {
+    meuVideo.volume -= 0.1;
+}
+
+function mute() {
+    if(meuVideo.muted == false)
+    meuVideo.muted  = true;
+    else
+    meuVideo.muted = false;
+}
+
+function opiniao(){
+    var x;
+    var nome;
+    var y;
+
+    var nome=prompt("Digite seu Nome"); 
+    var y=prompt("Digite sua idade ")
+    var x = prompt("Digite sua avaliação sobre nos")
+
+        alert(" Olá " + nome + " Você deu " + x + " estrelas \n Muito Obrigado!  :)");
+
+}
+
+
+
+//Video2
+
+var meuVideo2 =  document.getElementById("playerCustomizado2");
+
+function playPause2() {
+    if(meuVideo2.paused)
+     meuVideo2.play();
+    else
+    meuVideo2.pause();
+}
+
+function ampliar2() {
+    meuVideo2.width = 650;
+}
+
+function reduzir2() {
+    meuVideo2.width= 350;
+}
+
+function telaCheia2() {
+    meuVideo2.requestFullscreen (); 
+}
+function tamanhoNormal2() {
+    meuVideo2.width=500;
+}
+
+function aumentarVolume2() {
+    meuVideo2.volume +=0.1;
+}
+
+function diminuirVolume2() {
+    meuVideo2.volume -= 0.1;
+}
+
+function mute2() {
+    if(meuVideo2.muted == false)
+    meuVideo2.muted  = true;
+    else
+    meuVideo2.muted = false;
+}
+
+function opiniao2(){
+    var x;
+    var nome;
+    var y;
+
+    var nome=prompt("Digite seu Nome"); 
+    var y=prompt("Digite sua idade ")
+    var x = prompt("Digite sua avaliação sobre nos")
+
+        alert(" Olá " + nome + " Você deu " + x + " estrelas \n Muito Obrigado!  :)");
+
+}
+
+
+var meuVideo3 =  document.getElementById("playerCustomizado3"); 
+
+
+function playPause3() {
+    if(meuVideo3.paused)
+     meuVideo3.play();
+    else
+    meuVideo3.pause();
+}
+
+function ampliar3() {
+    meuVideo3.width = 650;
+}
+
+function reduzir3() {
+    meuVideo3.width= 350;
+}
+
+function telaCheia3() {
+    meuVideo3.requestFullscreen (); 
+}
+function tamanhoNormal3() {
+    meuVideo3.width=500;
+}
+
+
+function aumentarVolume3() {
+    meuVideo3.volume +=0.1;
+}
+
+function diminuirVolume3() {
+    meuVideo3.volume -= 0.1;
+}
+
+function mute3() {
+    if(meuVideo3.muted == false)
+    meuVideo3.muted  = true;
+    else
+    meuVideo3.muted = false;
+}
+
+function opiniao3(){
+    var x;
+    var nome;
+    var y;
+
+    var nome=prompt("Digite seu Nome"); 
+    var y=prompt("Digite sua idade ")
+    var x = prompt("Digite sua avaliação sobre nos")
+
+        alert(" Olá " + nome + " Você deu " + x + " estrelas \n Muito Obrigado!  :)");
+
+}
+
+//video 4
+
+var meuVideo4 =  document.getElementById("playerCustomizado4"); 
+
+
+function playPause4() {
+    if(meuVideo4.paused)
+     meuVideo4.play();
+    else
+    meuVideo4.pause();
+}
+
+function ampliar4() {
+    meuVideo4.width = 650;
+}
+
+function reduzir4() {
+    meuVideo4.width= 350;
+}
+
+function telaCheia4() {
+    meuVideo4.requestFullscreen (); 
+}
+
+function aumentarVolume4() {
+    meuVideo4.volume +=0.1;
+}
+
+function diminuirVolume4() {
+    meuVideo4.volume -= 0.1;
+}
+function tamanhoNormal4() {
+    meuVideo4.width=500;
+}
+
+function mute4() {
+    if(meuVideo4.muted == false)
+    meuVideo4.muted  = true;
+    else
+    meuVideo4.muted = false;
+}
+
+function opiniao4(){
+    var x;
+    var nome;
+    var y;
+
+    var nome=prompt("Digite seu Nome"); 
+    var y=prompt("Digite sua idade ")
+    var x = prompt("Digite sua avaliação sobre nos")
+
+        alert(" Olá " + nome + " Você deu " + x + " estrelas \n Muito Obrigado!  :)");
+
+}
+
+
+
+
+
+
